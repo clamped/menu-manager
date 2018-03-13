cbb5073ab4d0438f197bd824dbcbc90509ae7ec0
Philip Clamp <phil@clamped.me.uk>
2018-03-13 20:35:23 +0000

    Update deployment to include dependencies

--- api/deploy-api.sh
+++ api/deploy-api.sh
@@ -6,17 +6,21 @@ pushd $(pwd)
 
 command -v aws || { echo "I require aws but it's not installed. Aborting." >&2; exit 1; }
 
-if [[ $PWD = */api ]]; then cd ..; fi;
+if [[ $PWD != */api ]]; then cd api; fi;
 
 npm install
 
+zip -r menu-manager-api.zip index.js node_modules/**
+
+aws s3 cp menu-manager-api.zip s3://menu-manager-api/menu-manager-api.zip
+
 aws cloudformation package \
-    --template-file ./api/deploy-api.yaml \
-    --output-template-file ./api/serverless-api-output.yaml \
+    --template-file ./deploy-api.yaml \
+    --output-template-file ./serverless-api-output.yaml \
     --s3-bucket menu-manager-api
 
 aws cloudformation deploy \
-   --template-file ./api/serverless-api-output.yaml \
+   --template-file ./serverless-api-output.yaml \
    --stack-name menu-manager-api \
    --capabilities CAPABILITY_IAM
 
---
+++ api/index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const tableName = process.env.TABLE_NAME;
+
+const createResponse = (statusCode, body) => {
+  return {
+    statusCode: statusCode,
+    body: body
+  }
+};
+
+exports.getRecipe = (event, context, callback) => {
+  let params = {
+    TableName: tableName,
+    Key: {
+      id: event.pathParameters.recipeId
+    }
+  };
+
+  let dbGet = (params) => { return dynamo.get(params).promise() };
+
+  dbGet(params).then((data) => {
+    if (!data.Item) {
+      callback(null, createResponse(404, "ITEM NOT FOUND"));
+      return;
+    }
+    console.log(`RETRIEVED ITEM SUCCESSFULLY WITH doc = ${data.Item.doc}`);
+    callback(null, createResponse(200, data.Item.doc));
+  }).catch((err) => {
+    console.log(`GET ITEM FAILED FOR doc = ${params.Key.id}, WITH ERROR: ${err}`);
+    callback(null, createResponse(500, err));
+  });
+};
+
+exports.addRecipe = (event, context, callback) => {
+  let item = {
+    id: uuid.v1(),
+    doc: event.body
+  };
+
+  let params = {
+    TableName: tableName,
+    Item: item
+  };
+
+  let dbPut = (params) => { return dynamo.put(params).promise() };
+
+  dbPut(params).then((data) => {
+    console.log(`PUT ITEM SUCCEEDED WITH doc = ${item.doc}`);
+    callback(null, createResponse(201, null));
+  }).catch((err) => {
+    console.log(`PUT ITEM FAILED FOR doc = ${item.doc}, WITH ERROR: ${err}`);
+    callback(null, createResponse(500, err));
+  });
+};
+
+exports.updateRecipe = (event, context, callback) => {
+  let item = {
+    id: event.pathParameters.recipeId,
+    doc: event.body
+  };
+
+  let params = {
+    TableName: tableName,
+    Item: item
+  };
+
+  let dbUpdate = (params) => { return dynamo.update(params).promise() };
+
+  dbUpdate(params).then((data) => {
+    console.log(`UPDATE ITEM SUCCEEDED WITH doc = ${item.doc}`);
+    callback(null, createResponse(200, null));
+  }).catch((err) => {
+    console.log(`UPDATE ITEM FAILED FOR doc = ${item.doc}, WITH ERROR: ${err}`);
+    callback(null, createResponse(500, err));
+  });
+};
+
+exports.deleteRecipe = (event, context, callback) => {
+  let params = {
+    TableName: tableName,
+    Key: {
+      id: event.pathParameters.recipeId
+    },
+    ReturnValues: 'ALL_OLD'
+  };
+
+  let dbDelete = (params) => { return dynamo.delete(params).promise() };
+
+  dbDelete(params).then((data) => {
+    if (!data.Attributes) {
+      callback(null, createResponse(404, "ITEM NOT FOUND FOR DELETION"));
+      return;
+    }
+    console.log(`DELETED ITEM SUCCESSFULLY WITH id = ${event.pathParameters.recipeId}`);
+    callback(null, createResponse(200, null));
+  }).catch((err) => {
+    console.log(`DELETE ITEM FAILED FOR id = ${event.pathParameters.recipeId}, WITH ERROR: ${err}`);
+    callback(null, createResponse(500, err));
+  });
+};
